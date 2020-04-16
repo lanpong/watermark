fc2a392c61666b03a35998583cbd6cdfa3375a01
0x0ffff <oxoffff@outlook.com>
2020-04-16 16:17:23 +0800

    modify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="home">
-    <img :src="imageUrl" />
+    <img :src="markedImg" />
     <div>保存</div>
     <button @click="onPickFile">选择图片</button>
-    <input type="file" ref="fileInput" accept="image/*" @change="getFile" style="display: none" />
+    <input
+      type="file"
+      ref="fileInput"
+      accept="image/*"
+      @change="handleImgChange"
+      style="display: none"
+    />
   </div>
 </template>
 
 <script>
 import defaultpng from "../assets/default.png";
-import Watermark from "watermarkjs";
+// import Watermark from "watermarkjs";
 
 export default {
   name: "home",
   data() {
     return {
-      imageUrl: defaultpng,
-      markedImg: "",
-      markedAlpha: 50,
-      markedColor: "#fff",
-      markedSize: 16,
-      markedText: "这是一个小水印",
+      markedImg: defaultpng,
+      markedAlpha: 50, //
+      markedColor: "#fff", // 颜色
+      markedSize: 16, // 字号
+      markedText: "这是一个小水印", // 内容
       rawImg: "",
       controlDisabled: true
     };
@@ -29,19 +34,16 @@ export default {
     onPickFile() {
       this.$refs.fileInput.click();
     },
-    getFile(event) {
-      const files = event.target.files;
-      let filename = files[0].name; //只有一个文件
-      if (filename.lastIndexOf(".") <= 0) {
-        return alert("Please add a valid image!"); //判断图片是否有效
-      }
-      const fileReader = new FileReader(); //内置方法new FileReader()   读取文件
-      fileReader.addEventListener("load", () => {
-        this.imageUrl = fileReader.result;
-      });
-      fileReader.readAsDataURL(files[0]);
-      this.image = files[0];
-      //到这里后, 选择图片就可以显示出来了
+    handleImgChange(e) {
+      const imgFile = e.target.files[0];
+      this.rawImg = imgFile;
+
+      const reader = new FileReader();
+      reader.readAsDataURL(imgFile);
+      reader.onload = arg => {
+        this.markedImg = arg.target.result;
+        this.controlDisabled = false;
+      };
     }
   }
 };
