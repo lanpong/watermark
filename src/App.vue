a2a90fc78de24d100235cc9900caabcda04cb7bd
Chef5 <nunet@qq.com>
2023-05-01 00:30:47 +0800

    feat: 样式优化调整

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script setup>
 import defaultpng from "./assets/default.png";
+import github from "./assets/github.svg";
 import Watermark from "watermarkjs";
 import download from "./utils/download";
 import { onMounted, reactive, ref } from "vue";
 import "element-plus/es/components/message/style/css";
 import { ElMessage } from "element-plus";
+import project from "../package.json";
+
+const version = project.version;
 
 const markedImg = ref(defaultpng);
 const originImg = ref(defaultpng);
 
 let markedAlpha = ref(50); // 透明度
-let markedColor = ref("#ffffff"); // 水印的颜色
+let markedColor = ref("#c3c3c3"); // 水印的颜色
 let markedCount = ref(50); // 水印的数量
 let markedSize = ref(24); // 字体大小
 let markedText = ref("仅用于xxxxx使用"); // 水印的文字
@@ -21,6 +25,7 @@ const downloadExtTypeList = reactive(["png", "jpg"]);
 const predefineColors = reactive([
   "#000000",
   "#ffffff",
+  "#c3c3c3",
   "#ff4500",
   "#ff8c00",
   "#ffd700",
@@ -51,6 +56,10 @@ const options = reactive([
 ]);
 let markedFont = ref("Arial"); // 水印字体
 const fileInput = ref(); // 需要添加水印的图片
+
+function onGithub() {
+  window.open(project.homepage, '_blank');
+}
 
 function onPickFile() {
   fileInput.value.click();
@@ -184,139 +193,164 @@ onMounted(() => {
 
 <template>
   <div class="home">
+    <!-- 图片 -->
     <div class="imgcontent">
-      <img :src="markedImg" />
-      <el-button @click="onPickFile">选择图片</el-button>
+      <img :src="markedImg" @click="onPickFile" />
+      <el-button type="primary" v-if="!markedImg" @click="onPickFile">选择图片</el-button>
       <input type="file" ref="fileInput" accept="image/*" @change="handleImgChange" style="display: none" />
     </div>
-    <div class="text">
-      <el-input v-model="markedText" placeholder="请输入内容" @input="__marked"></el-input>
-    </div>
-    <div class="fontstyle">
-      <span>字体 / 颜色：</span>
-      <el-select v-model="markedFont" @change="__marked" placeholder="请选择">
-        <el-option v-for="item in options" :key="item.markedFont" :label="item.label" :value="item.markedFont"></el-option>
-      </el-select>
-      <el-color-picker v-model="markedColor" :predefine="predefineColors" @change="__marked"></el-color-picker>
-    </div>
-
-    <div class="other">
-      <div class="block">
-        <span class="demonstration">水印数量</span>
-        <el-slider v-model="markedCount" :min="40" :max="600" @change="__marked"></el-slider>
+    <!-- 工具箱 -->
+    <div class="tool">
+      <img class="github" :src="github" alt="github/Chef5" @click="onGithub">
+      <h2>证件水印 <span>v{{ version }}</span></h2>
+      <div class="tool-item">
+        <span class="item-label">图片</span>
+        <el-button plain type="primary" @click="onPickFile">{{ markedImg ? '更换图片' : '选择图片' }}</el-button>
       </div>
-      <div class="block">
-        <span class="demonstration">透明度</span>
-        <el-slider v-model="markedAlpha" @change="__marked"></el-slider>
+      <div class="tool-item">
+        <span class="item-label">水印内容</span>
+        <el-input type="textarea" class="item-value" v-model="markedText" placeholder="请输入内容" @input="__marked"></el-input>
       </div>
-      <div class="block">
-        <span class="demonstration">字体大小</span>
-        <el-slider v-model="markedSize" @change="__marked"></el-slider>
+      <div class="tool-item">
+        <span class="item-label">字体</span>
+        <div class="item-value">
+          <el-select v-model="markedFont" @change="__marked" placeholder="请选择">
+            <el-option v-for="item in options" :key="item.markedFont" :label="item.label" :value="item.markedFont"></el-option>
+          </el-select>
+        </div>
       </div>
-      <div class="block">
-        <span class="demonstration">角度</span>
-        <el-slider v-model="markedRotate" :min="0" :max="90" @change="__marked"></el-slider>
+      <div class="tool-item">
+        <span class="item-label">颜色</span>
+        <el-color-picker class="item-value" v-model="markedColor" :predefine="predefineColors" @change="__marked"></el-color-picker>
       </div>
-      <div class="block">
-        <span class="demonstration">间隔</span>
-        <el-slider v-model="markedGap" :min="32" :max="100" @change="__marked"></el-slider>
+      <div class="tool-item">
+        <span class="item-label">水印数量</span>
+        <el-slider class="item-value" v-model="markedCount" :min="40" :max="600" @change="__marked"></el-slider>
       </div>
-      <div class="block">
-        <span class="demonstration">保存类型</span>
-        <div class="el-slider" style="text-align: left">
+      <div class="tool-item">
+        <span class="item-label">透明度</span>
+        <el-slider class="item-value" v-model="markedAlpha" @change="__marked"></el-slider>
+      </div>
+      <div class="tool-item">
+        <span class="item-label">字体大小</span>
+        <el-slider class="item-value" v-model="markedSize" @change="__marked"></el-slider>
+      </div>
+      <div class="tool-item">
+        <span class="item-label">角度</span>
+        <el-slider class="item-value" v-model="markedRotate" :min="0" :max="90" @change="__marked"></el-slider>
+      </div>
+      <div class="tool-item">
+        <span class="item-label">间隔</span>
+        <el-slider class="item-value" v-model="markedGap" :min="32" :max="100" @change="__marked"></el-slider>
+      </div>
+      <div class="tool-item">
+        <span class="item-label">保存类型</span>
+        <div class="item-value">
           <el-select v-model="downloadExtType" placeholder="请选择">
             <el-option v-for="item in downloadExtTypeList" :key="item" :label="item" :value="item"></el-option>
           </el-select>
         </div>
       </div>
-    </div>
-    <div class="save">
-      <!-- <el-button @click="__markWater">添加水印</el-button> -->
-      <el-button @click="__saveSettings">保存配置</el-button>
-      <el-button @click="__download">保存图片</el-button>
+
+      <div class="save">
+        <!-- <el-button @click="__markWater">添加水印</el-button> -->
+        <el-button @click="__saveSettings">保存配置</el-button>
+        <el-button type="primary" @click="__download">保存图片</el-button>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .home {
+  width: 100%;
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
   .imgcontent {
-    img {
-      width: 500px;
-      display: block;
-    }
-    .el-button {
-      margin-top: 20px;
-    }
-  }
-  .text {
-    width: 650px;
-    margin-top: 20px;
-    .el-input {
-      width: 80%;
-    }
-  }
-  .fontstyle {
+    width: 0;
+    height: 100%;
+    flex: 1;
     display: flex;
     align-items: center;
-    justify-content: center;
-    width: 650px;
-    margin-top: 20px;
-    span {
-      font-size: 14px;
-      color: #8492a6;
-      line-height: 44px;
-    }
-    .el-select {
-      width: 40%;
-      margin: 0 20px;
+    border-right: 1px dashed #ccc;
+    img {
+      width: 100%;
+      display: block;
     }
   }
-  .other {
-    width: 650px;
-    border-top: 1px solid #eff2f6;
-    border-right: 1px solid #eff2f6;
-    border-left: 1px solid #eff2f6;
-    margin-top: 20px;
-    margin-bottom: 20px;
-    .block {
-      padding: 20px 10px;
-      overflow: hidden;
+  .tool {
+    position: relative;
+    box-sizing: border-box;
+    padding: 0 20px 20px;
+    width: 350px;
+    height: 100%;
+    .tool-item {
+      display: flex;
+      align-items: center;
       border-bottom: 1px solid #eff2f6;
-      .demonstration {
+      .item-label {
+        display: inline-block;
+        padding-right: 20px;
+        width: 60px;
+        text-align: right;
         font-size: 14px;
         color: #8492a6;
         line-height: 42px;
       }
-      .demonstration + .el-slider {
-        height: 42px;
-        float: right;
+      .item-value {
+        padding: 5px 0;
         width: 70%;
-        margin-right: 20px;
+        float: right;
+        display: flex;
+        align-items: center;
+
+        .el-select {
+          margin: 0;
+        }
+        .el-slider {
+          padding: 0;
+        }
       }
+    }
+    .el-select {
+      width: 40%;
+      margin: 0 10px;
+    }
+    .github {
+      position: absolute;
+      right: 10px;
+      top: 10px;
+      width: 30px;
+      height: 30px;
+    }
+    h2 {
+      span {
+        font-size: 14px;
+        color: #666;
+      }
+    }
+    .save {
+      margin-top: 20px;
     }
   }
 }
 @media screen and (max-width: 750px) {
   .home {
+    height: 100%;
+    flex-direction: column;
+    overflow: hidden;
     .imgcontent {
-      img {
-        width: 100%;
-        display: block;
-      }
-    }
-    .text {
       width: 100%;
+      height: max-content;
+      flex: 0;
+      border-right: 0;
     }
-    .fontstyle {
+    .tool {
       width: 100%;
-    }
-    .other {
-      width: 100%;
+      flex: 1;
+      overflow-y: auto;
     }
   }
 }
